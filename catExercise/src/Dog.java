cc2b9a988e8f91ad39c49600eaa9974c3dd10801
Darren Daniel <127184690+darren2812@users.noreply.github.com>
2025-09-21 23:13:02 -0700

    Update Dog.java

--- catExercise/src/Dog.java
+++ catExercise/src/Dog.java
@@ -1,8 +1,14 @@
 public class Dog {
-    String name;
-    int age;
+    String name = "-";
+    int age = 0;
+    public static int totalDogs = 0;
     public Dog(String name, int age){
         this.name = name;
         this.age = age;
+        totalDogs++;
+    }
+    public static int getTotalDogs(){
+        return totalDogs;
     }
 }
+
